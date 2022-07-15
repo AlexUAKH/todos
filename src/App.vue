946546d12488380bf45a7d9d3fbde2c98fcf7638
AlexUAKH <sashaos1404@gmail.com>
2022-07-15 12:49:12 +0300

    start project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <!--  <Layout />-->
+  <div class="_container">
+    <BaseButton>apply now</BaseButton>
+    <BaseInput label="Name" />
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+import BaseButton from "./components/base/BaseButton.vue";
+import BaseInput from "./components/base/BaseInput.vue";
+// import { useRoute } from "vue-router";
+// import { computed, defineAsyncComponent } from "vue";
+//
+// const route = useRoute();
+// const Layout = computed(() => {
+//   if (route.meta && route.meta.layout === "main") {
+//     return defineAsyncComponent(() => import("@/layout/MainLayout"));
+//   } else {
+//     return defineAsyncComponent(() => import("@/layout/DefaultLayout"));
+//   }
+// });
+</script>
+
+<!--<style scoped>-->
+<!--</style>-->
