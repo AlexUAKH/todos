9efcea1a2a0315986ececb5d040b28db38163938
AlexUAKH <sashaos1404@gmail.com>
2022-07-17 17:58:26 +0300

    icon components created from svg

---
+++ src/components/icons/IconDelete.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    aria-hidden="true"
+    role="img"
+    class="iconify iconify--mdi"
+    width="24"
+    height="24"
+    preserveAspectRatio="xMidYMid meet"
+    viewBox="0 0 24 24"
+    :style="{ cursor: 'pointer' }"
+  >
+    <path
+      d="M19 6.41 17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"
+      fill="red"
+    ></path>
+  </svg>
+</template>
---
+++ src/components/icons/IconDone.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    aria-hidden="true"
+    role="img"
+    class="iconify iconify--mdi"
+    width="24"
+    height="24"
+    preserveAspectRatio="xMidYMid meet"
+    viewBox="0 0 24 24"
+    :style="{ cursor: 'pointer' }"
+  >
+    <path
+      d="M9 16.2 4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"
+      fill="green"
+    ></path>
+  </svg>
+</template>
